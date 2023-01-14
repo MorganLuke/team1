ce08fd9df7cc130fa5b197b90bae4eba20bf18ef
JDeVoldemort <dragonlore8@gmail.com>
2023-01-13 23:32:28 -0700

    quantity comment cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,11 +1,10 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  let cart_list = [];
 
   if (localStorage.getItem("so-cart")) { 
     const cartItems = getLocalStorage("so-cart");
-  
+  // possibly use cartItems array to find duplicate products to add up quantity to put in to html.
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   }
