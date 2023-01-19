ecf04ee634fa1b9234aff4c65e2042097a904c6b
JDeVoldemort <dragonlore8@gmail.com>
2023-01-19 13:32:43 -0700

    branch mods

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import {ProductDetails, addProductToCart} from "./ProductDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 const dataSource = new ProductData("tents");
 
-const productId = getParam('product');
+const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
