60053edabca1df34d6ec5ed9ebab74d2647866b5
morganluke <morganluke@gmail.com>
2023-01-19 14:17:29 -0800

    removed add product to cart from import

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,7 @@
 import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import {ProductDetails, addProductToCart} from "./ProductDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
 const dataSource = new ProductData("tents");
 
 const productId = getParam('product');
