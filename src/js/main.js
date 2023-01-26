865588696d130d241af43c376c77a34988ec5519
thankgodandrew1 <and22046@byui.edu>
2023-01-26 10:18:29 -0700

    Complete! "Products Listing Templates"

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import ProductData from "./ProductDetails.mjs";
+import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("Tents");
