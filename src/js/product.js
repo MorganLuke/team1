72b829628fb0802f08dff467c0a0fc2a9cd789cd
JDVoldemort <dragonlore8@gmail.com>
2023-02-08 00:03:42 -0700

    stuck currently with object showing up in al

--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,7 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+import Alert from "./alert.mjs";
 
 loadHeaderFooter();
 const dataSource = new ProductData("tents");
@@ -10,5 +11,7 @@ const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+const alert = new Alert();
+alert.processAlert();
 
 // console.log(dataSource.findProductById(productId));
