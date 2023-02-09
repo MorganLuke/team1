0e9a9e21acb3ea190896bafe6f3b479dbc1c0528
JDVoldemort <dragonlore8@gmail.com>
2023-02-09 13:39:50 -0700

    run format and lint

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,7 +4,7 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category = getParam('category');
+const category = getParam("category");
 const dataSource = new ProductData();
 const element = document.querySelector(".product-list");
 const listing = new ProductList(category, dataSource, element);
--- src/js/product.js
+++ src/js/product.js
@@ -5,7 +5,7 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category = getParam('category');
+const category = getParam("category");
 const dataSource = new ProductData("tents");
 
 const productId = getParam("product");
