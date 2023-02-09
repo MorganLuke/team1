a301705d4ead5332f1c2d6981136e8ed6419ced6
morganluke <morganluke@gmail.com>
2023-02-09 12:23:39 -0800

    week 6 team activity

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -6,15 +6,15 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
     let discount = product.SuggestedRetailPrice - product.FinalPrice
     return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
+    <a href="/product_pages/index.html?product=${product.Id}">
     <img
-      src="${product.Image}"
+      src="${product.Images.PrimaryMedium}"
       alt="Image of ${product.Name}"
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.Name}</h2>
     <p class="product-card__price">$${product.FinalPrice}</p>
-    <p class="product_discount"> Discount: -$${discount.toFixed(2)}</p>
+    <p class="product_discount"> Discount: $${discount.toFixed(2)}</p>
     </a>
     
   </li>`;
@@ -29,9 +29,11 @@ export default class ProductList {
     }
     async init() {
         // our dataSource will return a Promise...so we can use await to resolve it.
-        const list = await this.dataSource.getData();
+        // const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         // render the list 
         this.renderList(list);
+        document.querySelector(".title").innerHTML = this.category;
       }
     renderList(list) {
         renderListWithTemplate(productCardTemplate, this.listElement, list);
--- src/js/product.js
+++ src/js/product.js
@@ -4,11 +4,13 @@ import ProductDetails from "./ProductDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+const category = getParam('category');
 const dataSource = new ProductData("tents");
 
 const productId = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
+const product = new ProductDetails(category, productId, dataSource);
 product.init();
 
 // console.log(dataSource.findProductById(productId));
