721c84e294c6d8b8d799e208c000978f01b0533e
thankgodandrew1 <and22046@byui.edu>
2023-02-11 10:37:14 -0700

    added product search functionality

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -16,7 +16,7 @@ export default class ProductData {
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
-    console.log(data.Result);
+    // console.log(data.Result);
     return data.Result;
   }
   // getData() {
@@ -31,4 +31,34 @@ export default class ProductData {
     // const products = await this.getData();
     // return products.find((item) => item.Id === id);
   }
+  // This is an async method called "searchAllCategories" that accepts a string parameter "searchString"
+  // async searchAllCategories(searchString) {
+  //   let categories = ["tents", "backpacks", "hammocks", "sleeping-bags"]; // list of all categories or Defines an array of category names
+  //   let searchResults = []; // Define an empty array to store the searchResult
+
+  // // Iterate through each category in the "categories" array using a "for...of" loop
+  // for (let category of categories) {
+  //   const data = await this.getData(category); // Call the "getData" method of the "this" object and pass the current category as an argument to get data for that category
+    
+  //   // Use the "filter" method to filter the data by checking if the "Name" property of each item contains the search string (case-insensitive)
+  //   // Then use the "spread" operator to append the filtered data to the "searchResults" array
+  //   // searchResults.push(...data.filter(item => item.Name.toLowerCase().includes(searchString.toLowerCase()))); 
+  //   let matchedProduct = data.filter(element => 
+  //     element.Name.toLowerCase().includes(`${searchString}`));
+  //     for (const item of matchedProduct) {
+  //       searchResults.push(item)
+  //     }
+  //     // console.log(matchedProduct)
+  // }
+
+  // return searchResults; // Return the array of filtered results
+
+  // }
+
+  async searchCategory(category, searchString) {
+    const data = await this.getData(category);
+    const searchResults = data.filter(item => item.Name.toLowerCase().includes(searchString.toLowerCase()));
+    return searchResults;
+  }
+  
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,55 @@
+import { loadHeaderFooter, getLocalStorage, setLocalStorage, renderListWithTemplate } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+import { productCardTemplate } from "./ProductList.mjs"
 
-loadHeaderFooter();
+loadHeaderFooter().then(() => {
+  document.forms[0].addEventListener("submit", () => {
+    let searchString = document.querySelector("#search-input").value;
+    setLocalStorage("search-string", searchString.trim());
+    renderSearchResult(searchString.trim().toLowerCase()); // call renderSearchResult function after form submission
+  });
+//   const searchString = getLocalStorage("search-string").toLowerCase();
+//   const dataSource = new ProductData();
+//   const listUl = document.querySelector(".product-list"); // Enclose the selector in quotes
+//   const titleHtml = document.querySelector(".title");
 
-const dataSource = new ProductData("Tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("tents", dataSource, element);
-
-listing.init();
-// console.log(list);
+//   async function renderSearchResult(searchString) {
+//     // console.log(searchString)
+//     const searchResults = await dataSource.searchAllCategories(searchString);
+//     const searchResultsHtml = renderListWithTemplate(productCardTemplate, listUl, searchResults);
+//     listUl.innerHTML = searchResultsHtml;
+//     // titleHtml.innerHTML = `Search Results for "${searchString}"`;
+//     titleHtml.innerHTML = `Showing ${searchResults.length} results for "${searchString}"`
+//   }
+  async function renderSearchResult(searchString) {
+    const dataSource = new ProductData();
+    const listUl = document.querySelector(".product-list");
+    const titleHtml = document.querySelector(".title");
+    let searchResults = [];
+  
+    // Search "tents" category separately
+    searchResults = searchResults.concat(await dataSource.searchCategory("tents", searchString));
+  
+    // Search other categories
+    const categories = ["backpacks", "hammocks", "sleeping-bags"];
+    for (let category of categories) {
+      searchResults = searchResults.concat(await dataSource.searchCategory(category, searchString));
+    }
+  
+    if (searchResults.length === 0) {
+      listUl.innerHTML = "No results found.";
+    } else {
+      renderListWithTemplate(productCardTemplate, listUl, searchResults);
+      // listUl.innerHTML = searchResultsHtml;
+      titleHtml.innerHTML = `Showing ${searchResults.length} results for "${searchString}"`
+    }
+  }
+  
+  if (window.location.href.includes("/product-listing/product-search.html")) {
+    console.log("Page redirected correctly");
+    const searchString = getLocalStorage("search-string").toLowerCase(); // set searchString variable
+    renderSearchResult(searchString);
+  } else {
+    console.log("Page redirect failed");
+  }
+});
