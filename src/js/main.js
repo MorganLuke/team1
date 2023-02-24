bb5831b7173ec11646a92ac6ed0ea52734f37861
thankgodandrew1 <and22046@byui.edu>
2023-02-24 13:24:27 -0700

    working on action register

--- src/js/main.js
+++ src/js/main.js
@@ -62,3 +62,29 @@ loadHeaderFooter().then(() => {
     console.log("Page redirect failed");
   }
 });
+
+// Get the modal element
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var registerBtn = document.getElementById("registerBtn");
+
+// Get the close button element
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+registerBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
